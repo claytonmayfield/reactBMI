600db7daf8ed471d874d8203a46db759b09e6d87
claytonmayfield <clayton_mayfield@insideranken.org>
2022-11-18 13:42:44 -0600

    bmi react app

--- src/App.js
+++ src/App.js
@@ -1,115 +1,122 @@
 import React, {useState} from 'react'
-import './App.css';
+
+import './index.css'
 
 function App() {
-// State Variables
-const [height, setHeight]   = useState(0)
-const [weight, setWeight]   = useState(0)
-const [bmi, setBmi]         = useState('')
-const [message, setMessage] = useState('')
+  const	MINHEIGHT =  12;
+  const MAXHEIGHT =  96;
+  const MINWEIGHT =   1;
+  const MAXWEIGHT = 777;
+  
+  // state
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
 
-const MINHEIGHT =12
-const MINWEIGHT = 1
-const MAXWEIGHT = 777
-const MAXHEIGHT = 96
+  let calculateBMI = (e) => {
+    //prevent submitting
+    e.preventDefault()
+
+    if (weight === 0 || height === 0) 
+	{
+      alert('Please enter a valid weight and height')
+    } 
+	else if ((height < MINHEIGHT || height > MAXHEIGHT)) 
+	{
+      alert('Please enter a valid height (12 - 96)')
+    }
+	else if ((weight < MINWEIGHT || weight > MAXWEIGHT)) 
+	{
+      alert('Please enter a valid height (1 - 777)')
+    }
+	else 
+	{
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+
+      // Logic for message
+      if (bmi < 18.5) 
+	  {
+        setMessage('You are underweight')
+      } 
+	  else if (bmi < 25.0) 
+	  {
+        setMessage('You are optimal weight')
+      }
+	  else if (bmi < 30.0) 
+	  {
+        setMessage('You are overweight')
+      }
+	  else if (bmi >= 30.0)
+	  {
+		setMessage('You are obese')
+	  }	  
+    }
+  }
+
+  //  show image based on bmi calculation
+  let imgSrc;
+
+  if (bmi < 1) 
+  {
+    imgSrc = null
+  } 
+  else if (bmi < 18.5) 
+  {
+	imgSrc = require('../src/assets/underweight.png')
+  } 
+  else if (bmi < 25.0) 
+  {
+	imgSrc = require('../src/assets/optimalweight.png')
+  }
+  else if (bmi < 30.0) 
+  {
+	imgSrc = require('../src/assets/overweight.png')
+  }
+  else if (bmi >= 30.0)
+  {
+	imgSrc = require('../src/assets/obese.png')
+  }	  
 
 
-let calculateBMI = (e) => {
-  e.preventDefault()
+  let reload = () => {
+    window.location.reload()
+  }
 
-  if((height === 0) || (weight === 0))
-{
-  alert("must enter a height and a weight")
-}
+  return (
+    <div className="app">
+      <div className=''>
+        <h2 className='center'>BMI Calculator</h2>
+        <form onSubmit={calculateBMI}>
+          <div>
+            <label className='heightWord'>Height (12 - 96)</label>
+            <input value={height} onChange={(event) => setHeight(event.target.value)} />
+          </div>
+          <br></br>
+          <div>
+            <label className='weightWord'>Weight (1 - 777)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
 
-else if ((height < MINHEIGHT) || (height > MAXHEIGHT))
-{
-  alert("Height must be between " + MINHEIGHT + "and" + MAXHEIGHT)
-}
+          <div>
+         <br></br>
+            <button id='submit' className=' btn btn-dark' type='submit'>Submit</button>
+            <button className='btn btn-dark' onClick={reload} type='submit'>Reload</button>
+          </div>
+        </form>
 
-else if ((weight < MINWEIGHT) || (weight > MAXWEIGHT))
-{
-  alert("weight must be between " + MINWEIGHT + "and" + MAXWEIGHT)
-}
-else {
-  let bmi = (weight / (Math.pow(height, 2)) * 703)
-  setBmi(bmi.toFixed(2))
-
-if (bmi < 18.5)
-{
-  setMessage("UnderWeight")
-}
-else if (bmi < 25.0)
-{
-  setMessage("Optimal weight")
-}
-else if (bmi < 30.0)
-{
-  setMessage("overweight")
-}
-else if (bmi >= 30.0)
-{
-  setMessage("obese")
-}
-
-let imgSrc
-
-if(bmi < 1)
-{
-  imgSrc = null
-}
-else if (bmi < 18.5)
-{ 
-  imgSrc = require('')
-}
-else if (bmi < 25.0)
-{
-  imgSrc = require('')
-}
-else if (bmi < 30.0)
-{
-  imgSrc = require('')
-}
-else if (bmi >= 30.0)
-{
-  imgSrc = require('')
-}
-
-}
-}
-let reload = () => {
-  window.location.reload()
-}
-let imgSrc
-return (
-  <div className="App">
-    <div className='container'>
-      <h2>React BMI Calculator</h2>
-      <form onSubmit={calculateBMI()}>
-        <div>
-        {/* evt Stands for event */}
-          <label>Height (12 - 96)</label>
-          <input value={height} onChange={(evt => setHeight(evt.target.value))}></input>
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
         </div>
-        <div>
-          <label>Weight (1 - 777)</label>
-          <input value={weight} onChange={(evt => setWeight(evt.target.value))}></input>
+
+        <div className='img-container'>
+          <img src={imgSrc} alt=''></img>
         </div>
-        <div>
-          <button className='btn' type='submit'>Submit</button>
-          <button className='btn' type='reset' onClick={reload()}>Clear</button>
-        </div>
-      </form>
-      <div>
-        <h3>Your BMI is: {bmi}</h3>
-        <p>{message}</p>
-      </div>
-      <div className='img-container'>
-        <img src={imgSrc} alt="Body Type Image"/>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
